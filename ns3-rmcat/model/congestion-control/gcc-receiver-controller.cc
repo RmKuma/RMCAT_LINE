309dc368e685bc13378d6e40cd98a5686e16187f
hsherlcok <sk1528@naver.com>
2018-07-26 07:54:25 +0000

    File Structure Modi

---
+++ ns3-rmcat/model/congestion-control/gcc-receiver-controller.cc
@@ -0,0 +1,99 @@
+/******************************************************************************
+ * Copyright 2016-2017 Cisco Systems, Inc.                                    *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ *                                                                            *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+/**
+ * @file
+ * Gcc Receiver Side Controller (Loss Based Congestion Control) implementation for gcc ns3 module.
+ *
+ * @version 0.1.0
+ */
+#include "gcc-receiver-controller.h"
+#include <sstream>
+#include <cassert>
+
+namespace rmcat {
+
+GccRecvController::GccRecvController() :
+    m_lastTimeCalcUs{0},
+    m_lastTimeCalcValid{false},
+    m_QdelayUs{0},
+    m_ploss{0},
+    m_plr{0.f},
+    m_RecvR{0.} {}
+
+GccRecvController::~GccRecvController() {}
+
+void GccRecvController::setCurrentBw(float newBw) {
+    m_initBw = newBw;
+}
+
+void GccRecvController::reset() {
+    m_lastTimeCalcUs = 0;
+    m_lastTimeCalcValid = false;
+
+    m_QdelayUs = 0;
+    m_ploss = 0;
+    m_plr = 0.f;
+    m_RecvR = 0.;
+}
+
+bool GccRecvController::processRembFeedback(uint64_t nowUs,
+                                      uint16_t sequence,
+                                      uint64_t rxTimestamp, uint8_t ecn){
+	// TODO Loss Based Controller
+}
+float GccRecvController::getBandwidth(uint64_t nowUs) const {
+
+    return m_initBw;
+}
+
+void GccRecvController::updateMetrics() {
+    uint64_t qdelayUs;
+    bool qdelayOK = getCurrentQdelay(qdelayUs);
+    if (qdelayOK) m_QdelayUs = qdelayUs;
+
+    float rrate;
+    bool rrateOK = getCurrentRecvRate(rrate);
+    if (rrateOK) m_RecvR = rrate;
+
+    uint32_t nLoss;
+    float plr;
+    bool plrOK = getPktLossInfo(nLoss, plr);
+    if (plrOK) {
+        m_ploss = nLoss;
+        m_plr = plr;
+    }
+}
+
+void GccRecvController::logStats(uint64_t nowUs) const {
+
+    std::ostringstream os;
+    os << std::fixed;
+    os.precision(RMCAT_LOG_PRINT_PRECISION);
+
+    os  << " algo:dummy " << m_id
+        << " ts: "     << (nowUs / 1000)
+        << " loglen: " << m_packetHistory.size()
+        << " qdel: "   << (m_QdelayUs / 1000)
+        << " ploss: "  << m_ploss
+        << " plr: "    << m_plr
+        << " rrate: "  << m_RecvR
+        << " srate: "  << m_initBw;
+    logMessage(os.str());
+}
+
+}
